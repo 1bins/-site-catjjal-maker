7716954af523b7e58ce81abdcf615709229ba441
wonbin Seo <1bins@kakao.com>
2023-12-17 23:43:39 +0900

    불필요한 파일 정리, CSS 위치 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './css/default/normalize.css';
+import './css/default/reset.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
